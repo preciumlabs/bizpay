93653073b67ae24df1bf82d4fec579217bdb78c1
preciumlabs <preciumlabs@gmail.com>
2024-07-13 11:31:16 +0100

    Commit
    Commit

---
+++ src/utils/ipfs.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-undef
+const { default: axios } = require('axios');
+
+const readMetadataFromIPFS = async path => {
+  let fileContent = await axios.get(path);
+  let status = fileContent.status;
+  if (status != 200) return '';
+  fileContent = fileContent.data;
+  return fileContent;
+};
+
+const IPFSHandler = {
+  readMetadataFromIPFS,
+};
+
+export default IPFSHandler;
